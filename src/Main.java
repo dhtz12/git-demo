ab91fc706bd20b92c195bad8b84a436480af926e
lisongsong2011 <lisongsong2011@sina.com>
2023-12-15 20:07:25 +0800

    init commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * packageName:
+ * User: lss
+ * Date: 2023/12/15
+ * desc:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( "Hello world!" );
+    }
+}
